a1a2ff44e3603636fa95c2278ec723d7b802c455
Tarunkanaujia <153187511+Tarunkanaujia@users.noreply.github.com>
2025-02-26 12:17:05 +0000

    update

---
+++ Array/Create.js
@@ -0,0 +1,21 @@
+// What is Array :- An array in JavaScript is a special variable that can hold 
+//                  multiple values in a single variable. 
+//                  It is a data structure used to store collections of data,
+//                  such as numbers, strings, objects, or even other arrays.
+//                  Arrays in JavaScript are dynamic, meaning they can grow and shrink as needed.
+
+
+// Creating Array
+// There are multiple way to create Array
+let emptyArr = [];
+let name = ["IRONMAN", "THOR", "HULK", "WANDA", "ANTMAN", "THANOS"];
+let marks = [23, 25, 68, 45.5];
+let mix = ["Titan", "Vormir", 25, 56]
+
+console.log(emptyArr);
+console.log(name);
+console.log(marks);
+console.log(mix);
+
+
+// In Javascript,We can store multiple values in array but not in java,C++
---
+++ Array/mutable.js
@@ -0,0 +1,5 @@
+// Arrays are mutable
+
+let name = ["DBMS","OS","COMPILER DESIGN","DATA STRUCTURE"]
+name[1] = "SYSTEM DESIGN"
+console.log(name);
